2cc1f14a05d838c845c23d3091d5d4687ba3e80f
dina-fouad <dina_albarghouthi@yahoo.com>
2021-07-26 18:18:11 +0300

    lab2

---
+++ math_series/series.py
@@ -0,0 +1,35 @@
+def fibonacci(n):
+
+        if n == 0:
+            return 0
+        if n==1:
+            return 1
+    
+        return fibonacci(n-1) + fibonacci(n-2) 
+#print(fibonacci(7))
+
+
+def lucas(n):
+
+        if n == 0:
+            return 2
+        if n==1:
+            return 1
+    
+        return lucas(n-1) + lucas(n-2) 
+#print(lucas(3))
+
+
+def sum_series(n,f=0,s=1):
+    
+    if n==0:
+        return f
+    if n==1:
+        return s
+   
+    return sum_series(n-1,f,s)+sum_series(n-2,f,s)
+
+
+       
+
+        
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,59 @@
 from math_series import __version__
-
+from math_series.series import  fibonacci,lucas,sum_series
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fibonacci():
+    expected = 0
+    actual =  fibonacci(0)
+    assert actual == expected
+
+def test_fibonacci2():
+    expected = 1
+    actual =  fibonacci(1)
+    assert actual == expected
+
+def test_fibonacci3():
+    expected = 1
+    actual = fibonacci(2)
+    assert actual == expected
+    
+def test_fibonacci5():
+    expected = 13
+    actual = fibonacci(7)
+    assert actual == expected
+
+
+def test_lucas():
+    expected = 2
+    actual = lucas(0)
+    assert actual == expected
+
+def test_lucas():
+    expected = 0
+    actual = lucas(1)
+    assert actual == expected    
+
+def test_lucas():
+    expected = 3
+    actual = lucas(2)
+    assert actual == expected    
+
+def test_lucas():
+    expected = 29
+    actual = lucas(7)
+    assert actual == expected
+
+def test_sum_series():
+    expected = 13
+    actual = sum_series(7,0,1)
+    assert actual == expected
+
+def test_sum_series1():
+    expected=0
+    actual=sum_series(0)
+    assert actual==expected    
+
+   
